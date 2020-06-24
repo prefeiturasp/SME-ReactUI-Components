ca221cb4611295f2feb7de2494ce85c23af77f68
Welligton <welligtonporto@gmail.com>
2020-06-24 18:50:44 -0300

    Corrigindo styles

--- src/components/Checkbox/styles.js
+++ src/components/Checkbox/styles.js
@@ -6,23 +6,23 @@ import { Checkbox } from 'antd';
 export const CheckboxStyle = styled(Checkbox)`
   .ant-checkbox-checked {
     .ant-checkbox-inner {
-      background-color: ${(props) => props.theme.primary};
-      border-color: ${(props) => props.theme.primary};
+      background-color: ${(props) => props.theme?.Colors?.Primary};
+      border-color: ${(props) => props.theme?.Colors?.Primary};
     }
 
     &::after {
-      border-color: ${(props) => props.theme.primary} !important;
+      border-color: ${(props) => props.theme?.Colors?.Primary} !important;
     }
   }
 
   &:hover {
     .ant-checkbox-inner {
-      border-color: ${(props) => props.theme.primary};
+      border-color: ${(props) => props.theme?.Colors?.Primary};
     }
   }
   
   .ant-checkbox-input:focus + .ant-checkbox-inner {
-    border-color: ${(props) => props.theme.primary};
+    border-color: ${(props) => props.theme?.Colors?.Primary};
   }
 
   .ant-checkbox + span {
@@ -32,8 +32,8 @@ export const CheckboxStyle = styled(Checkbox)`
   // Disabled
   .ant-checkbox-disabled {
     .ant-checkbox-inner {
-      background-color: ${(props) => props.theme.disabled};
-      border-color: ${(props) => props.theme.disabled} !important;
+      background-color: ${(props) => props.theme?.Colors?.disabled};
+      border-color: ${(props) => props.theme?.Colors?.disabled} !important;
     }
 
     &.ant-checkbox-checked {
