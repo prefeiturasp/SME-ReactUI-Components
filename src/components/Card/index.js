a3c4c310dad2ecc31cfe6b61128c6f5fe49a0a60
Ítalo Maio <italo.maio@amcom.com.br>
2020-06-30 14:30:23 -0300

    [AB#17203] Componente Card Storybook

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,45 +1,49 @@
 import React from 'react';
+import t from 'prop-types';
 
 // Estilos
-import {
-  CardStyle,
-  CardHeadStyle,
-  CardFootStyle,
-  CardBodyStyle,
-} from './styles';
+import { CardStyle } from './styles';
 
-function Card({ children, animate, borderLeft, borderBottom, ...rest }) {
+// Internal components
+import CardHeader from './components/CardHeader';
+import CardBody from './components/CardBody';
+import CardFooter from './components/CardFooter';
+
+/**
+ * `import { Card } from "@sme/secretui"`
+ *
+ * Um cartão é um padrão de design da interface do usuário que agrupa informações relacionadas em um contêiner de tamanho flexível. Um cartão é um contêiner para algumas informações curtas e relacionadas.
+ *
+ * # Boas práticas de uso
+ * - Os layouts de cartão são menos digitalizáveis ​​que as listas. Uma exibição de lista vertical padrão é mais digitalizável que os cartões, porque o posicionamento dos elementos individuais é fixo em tamanho e mais previsível para os olhos. Portanto, os cartões não são apropriados quando os usuários pesquisam um item específico de uma lista ou procuram um conteúdo específico.
+ * - Os cartões são uma excelente opção para aplicativos de painel que exibem uma variedade de tipos de conteúdo ao mesmo tempo na mesma página. Nessas situações, a metáfora do cartão pode ajudar a criar diferenças mais óbvias entre os itens, e cada cartão pode acomodar elementos diferentes.
+ */
+function Card({ children, animate, borderLeft, borderBottom, rounded }) {
   return (
     <CardStyle
       animate={animate}
       borderLeft={borderLeft}
       borderBottom={borderBottom}
-      {...rest}
+      rounded={rounded}
     >
       {children}
     </CardStyle>
   );
 }
 
-function CardHeader({ children, ...rest }) {
-  return <CardHeadStyle {...rest}>{children}</CardHeadStyle>;
-}
+Card.propTypes = {
+  animate: t.bool,
+  borderLeft: t.bool,
+  borderBottom: t.bool,
+  rounded: t.bool,
+};
 
-function CardBody({ children, ...rest }) {
-  return (
-    <CardBodyStyle bordered={false} {...rest}>
-      {children}
-    </CardBodyStyle>
-  );
-}
-
-function CardFooter({ children, ...rest }) {
-  return (
-    <CardFootStyle bordered={false} {...rest}>
-      {children}
-    </CardFootStyle>
-  );
-}
+Card.defaultProps = {
+  animate: true,
+  borderLeft: false,
+  borderBottom: false,
+  rounded: false,
+};
 
 Card.Header = CardHeader;
 Card.Body = CardBody;
---
+++ src/components/Card/index.stories.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// Components
+import { Card, Icon } from '~/components';
+
+const { Header, Body, Footer } = Card;
+
+export default {
+  title: 'Card',
+  component: Card,
+  subcomponents: { Header, Body, Footer },
+};
+
+const IconCard = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  font-size: 20px;
+  color: ${(props) => props.theme?.Colors?.Primary};
+  padding: 1rem 1rem;
+
+  i {
+    font-size: 100px;
+    margin-bottom: 1rem;
+  }
+`;
+
+export const CardStory = () => (
+  <Card rounded animate>
+    <Card.Header bordered={false}>Título</Card.Header>
+    <Card.Body>
+      <div>Conteúdo</div>
+      <div>Conteúdo</div>
+      <div>Conteúdo</div>
+      <div>Conteúdo</div>
+    </Card.Body>
+    <Card.Footer>
+      <a href="#">Novo pedido</a>
+    </Card.Footer>
+  </Card>
+);
+
+export const CardBorderLeftStory = () => (
+  <Card rounded animate borderLeft>
+    <Card.Header bordered={false}>Título</Card.Header>
+    <Card.Body>
+      <div>Conteúdo</div>
+      <div>Conteúdo</div>
+      <div>Conteúdo</div>
+      <div>Conteúdo</div>
+    </Card.Body>
+    <Card.Footer>
+      <a href="#">Novo pedido</a>
+    </Card.Footer>
+  </Card>
+);
+
+export const CardBorderBottomStory = () => (
+  <Card rounded animate borderBottom>
+    <Card.Body>
+      <IconCard>
+        <Icon type="solid" icon="fa-calendar-alt" />
+        Título
+      </IconCard>
+    </Card.Body>
+  </Card>
+);
+
+CardStory.story = {
+  name: 'default card',
+};
+
+CardBorderLeftStory.story = {
+  name: 'border left card',
+};
+
+CardBorderBottomStory.story = {
+  name: 'border bottom card',
+};
