2e385a94adfed1c427adde3bc05216eba65a8185
rayane maria dos santos <rayanemariadossantos@MacBook-Air-de-rayane.local>
2023-02-21 18:04:07 -0300

    Removendo codigo dep pacote desatualizado

--- examples/src/index.js
+++ examples/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import '../../index.css';
-import 'antd/dist/antd.css';
+
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import App from '~/App';
